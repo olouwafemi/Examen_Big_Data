3527e5ac6f1e95ab23188356825e806e0f5bbfe5
olouwafemi <rellaodounharo@gmail.com>
2021-05-20 13:06:09 +0000

    Troisième Commit

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -3,12 +3,22 @@ import scala.collection.immutable.List
 object ExamenBigData {
 
   def main(args: Array[String]): Unit = {
+    listeValeurs (List("Julien", "Paul", "Jean", "rac", "trec", "joel", "ed", "chris", "maurice"))
 
   }
 
   def listeValeurs (listText : List[String]) : List[String] = {
     val result : List[String] = listText.filter(l => l.endsWith("n"))
     return result
+  }
+
+  def list_double () : Unit = {
+
+    val liste : List[List[String]] = List(List("ecommercemag.fr", ""), List("https://www.journalducm.com/contact/" , "Payant"),
+      List("https://www.zatsaz.com/", ""), List("https://www.larevenu.com/", ""), List("https://www.cadre-dirigfdeant-magazine.com/", "Payant"),
+      List("https://www.silicon.fr/nous-contacter/", "Payant"), List ("https://www.itespresso.fr", ""), List ("https://www.industrie-mag.com/article4.html", "invité"),
+      List ("https://www.sjesuisundev.com/article-invite/",  "invité"), List ("https://www.numerama.com", ""))
+
   }
 
 }
